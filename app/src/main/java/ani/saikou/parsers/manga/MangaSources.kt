dc90febc85efde8bd962e81138299c90f06b32b6
tas33n <tasu.legend@gmail.com>
2023-10-11 01:09:52 +0600

    manga source fixed

--- app/src/main/java/ani/saikou/parsers/manga/MangaSources.kt
+++ app/src/main/java/ani/saikou/parsers/manga/MangaSources.kt
@@ -4,7 +4,19 @@ import ani.saikou.Lazier
 import ani.saikou.lazyList
 import ani.saikou.parsers.BaseParser
 import ani.saikou.parsers.MangaReadSources
-import ani.saikou.parsers.manga.sources.MangaDummy
+import ani.saikou.parsers.manga.sources.AllAnime
+import ani.saikou.parsers.manga.sources.ComickFun
+import ani.saikou.parsers.manga.sources.Manga4Life
+import ani.saikou.parsers.manga.sources.MangaBuddy
+import ani.saikou.parsers.manga.sources.MangaDex
+import ani.saikou.parsers.manga.sources.MangaHub
+import ani.saikou.parsers.manga.sources.MangaKakalot
+import ani.saikou.parsers.manga.sources.MangaKatana
+import ani.saikou.parsers.manga.sources.MangaPill
+import ani.saikou.parsers.manga.sources.MangaRead
+import ani.saikou.parsers.manga.sources.Manhwa18
+import ani.saikou.parsers.manga.sources.NHentai
+import ani.saikou.parsers.manga.sources.NineHentai
 
 object MangaSources : MangaReadSources() {
     override val list: List<Lazier<BaseParser>> = lazyList(
@@ -15,11 +27,11 @@ object MangaSources : MangaReadSources() {
 //        "MangaReaderTo" to ::MangaReaderTo,
         "AllAnime" to ::AllAnime,
         "Toonily" to ::Toonily,
-        "MangaHub" to ::MangaHub,
+//        "MangaHub" to ::MangaHub, cf blocked
         "MangaKatana" to ::MangaKatana,
         "Manga4Life" to ::Manga4Life,
         "MangaRead" to ::MangaRead,
-        "ComickFun" to ::ComickFun,
+//        "ComickFun" to ::ComickFun,
 
     )
 }
